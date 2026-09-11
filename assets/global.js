78d34608a27d9bf6565bc41ac614c9f2ee40fae1
Naqi-Haider <139011094+Naqi-Haider@users.noreply.github.com>
2026-09-11 23:33:42 +0500

    created the add button functionality

--- assets/global.js
+++ assets/global.js
@@ -25,3 +25,56 @@ class DeleteButton extends HTMLElement {
 }
 
 customElements.define('delete-button', DeleteButton);
+
+class QuantityInput extends HTMLElement {
+    constructor() {
+        super();
+        this.plusButton = this.querySelector('.plus');
+        this.minusButton = this.querySelector('.minus');
+        this.qtyInput = this.querySelector('input[type="number"]').valueAsNumber;
+        this.variantId = this.dataset.variantId;
+        //Or you can get the variant ID from the input field if you prefer
+        //this.variantId = this.getAttribute('data-variant-id');
+    }
+
+    connectedCallback() {
+        console.log("Quantity: ", this.qtyInput);
+
+        this.plusButton.addEventListener('click', async (e) => {
+            e.preventDefault();
+            console.log("plus");
+
+            const updatedValue = this.qtyInput + 1;
+            console.log("Updated Value: ", updatedValue, this.variantId);
+
+            let updates = {
+                [this.variantId]: updatedValue
+            };
+
+            try {
+                const res = await fetch(window.Shopify.routes.root + 'cart/update.js', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ updates })
+                })
+                // const data = await res.json();
+                // console.log("Updated Cart Data: ", data);
+
+                //To refresh the cart:
+                document.dispatchEvent(new CustomEvent('refresh:cart'));
+                
+            } catch (error) {
+                console.log("error plus: ",error);
+            }
+        })
+
+        this.minusButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            console.log("minus");
+        })
+    }
+}
+
+customElements.define('quantity-input', QuantityInput);
